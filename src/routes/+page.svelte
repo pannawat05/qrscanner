7b086b05dddeb6114ee2563c60025f6e0378e09e
pannawat05 <83331747+pannawat05@users.noreply.github.com>
2025-05-28 11:07:57 +0700

    finish

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,67 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<script>
+    import { Html5Qrcode } from 'html5-qrcode'
+    import { onMount } from 'svelte'
+
+    let scanning = false
+
+    let html5Qrcode
+
+    onMount(init)
+
+    function init() {
+        html5Qrcode = new Html5Qrcode('reader')
+    }
+
+    function start() {
+        html5Qrcode.start(
+            { facingMode: 'environment' },
+            {
+                fps: 10,
+                qrbox: { width: 250, height: 250 },
+            },
+            onScanSuccess,
+            onScanFailure
+        )
+        scanning = true
+    }
+
+    async function stop() {
+        await html5Qrcode.stop()
+        scanning = false
+    }
+
+    function onScanSuccess(decodedText, decodedResult) {
+        alert(`Code matched = ${decodedText}`)
+        console.log(decodedResult)
+    }
+
+    function onScanFailure(error) {
+        console.warn(`Code scan error = ${error}`)
+    }
+</script>
+
+<style>
+    main {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap: 20px;
+    }
+    reader {
+        width: 50%;
+        min-height: 400px;
+        margin-top: 5%;
+        background-color: black;
+    }
+</style>
+
+<main>
+    <reader id="reader"></reader>
+    {#if scanning}
+        <button class="w-80 h-15 bg-blue-300 p-8" on:click={stop}>stop</button>
+    {:else}
+        <button on:click={start} class="w-80 h-15 bg-blue-300">start</button>
+    {/if}
+</main>
